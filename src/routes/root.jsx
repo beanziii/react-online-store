c042b4cf673ddbceb23296e635cff38d4d329415
Brandon <93879641+beanziii@users.noreply.github.com>
2022-11-25 16:57:19 +0000

    Added categories

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,30 @@
 import useFetchItems from '../hooks/useFetchItems';
 
+const categories = [
+  'Electronics',
+  'Jewelery',
+  "Men's Clothing",
+  "Women's Clothing",
+];
+
 function Root() {
-  const { data, loading } = useFetchItems('jewelery');
+  // const { data, loading } = useFetchItems('jewelery');
+
+  const categoryItems = categories.map((category) => (
+    <li key={category} className="cursor-pointer text-2xl hover:underline">
+      {category}
+    </li>
+  ));
 
   return (
     <>
-      <h1>React Online Store</h1>
+      <span className="absolute bottom-8 right-8 bg-slate-900 px-8 py-4 text-white shadow-sm">
+        React Online Store
+      </span>
+
+      <div className="my-16">
+        <ul className="flex list-none flex-col gap-4">{categoryItems}</ul>
+      </div>
     </>
   );
 }
