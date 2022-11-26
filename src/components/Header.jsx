1aacd5aba6baef074458a54ec47fde4b754b0f1f
Brandon <93879641+beanziii@users.noreply.github.com>
2022-11-26 00:25:21 +0000

    added home link in nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 function Header({ links }) {
   const activeClassName = 'font-semibold underline';
 
   return (
     <header className="mb-10 flex items-center justify-between py-2">
-      <p className="text-xl font-bold">React Online Store</p>
+      <Link to="/" className="text-xl font-bold">
+        React Online Store
+      </Link>
 
       <nav>
         <ul className="flex items-center justify-between gap-2">
